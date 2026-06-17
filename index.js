c71879c11d3bf2a50f096215c6ef0f61ce405839
Fatima <fatima@pop-os.home>
2026-06-17 12:15:13 -0300

    feat: added visits counter

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
+let visitas = 0;
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.send("Hola mundo");
+    visitas++;
+
+    res.sendFile(
+        path.join(__dirname, "public", "index.html")
+    );
+});
+
+app.get("/visitas", (req, res) => {
+    res.json({
+        visitas: visitas
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+    console.log(
+        `Servidor ejecutándose en http://localhost:${PORT}`
+    );
 });
---
+++ public/script.js
@@ -0,0 +1,8 @@
+async function verVisitas() {
+
+    const respuesta = await fetch("/visitas");
+    const datos = await respuesta.json();
+
+    document.getElementById("resultado").innerText =
+        `Cantidad de visitas: ${datos.visitas}`;
+}
