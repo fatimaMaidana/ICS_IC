c7c16bc247b6895df1857f584fac164bcb5d660c
Fatima <fatima@pop-os.home>
2026-06-17 12:02:10 -0300

    feat: added express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hola mundo");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
